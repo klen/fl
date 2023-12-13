f3839e2a6a8cf440dc119636ea97942b229f5303
Kirill Klenov <horneds@gmail.com>
2023-12-13 17:53:32 +0300

    feat: wip

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
+  assetPrefix: isProd ? 'https://klen.github.io/fl/' : undefined,
   // output: 'standalone',
 };
 
